af1e48b5251ba38b102f399fb6a687db6ff00026
Madona Adeeb <madona.adeeb@hotmail.com>
2024-01-21 23:16:50 +0200

    Update Flask package to version 2.1.0
    Change Flask package version to 2.1.0 for compatibility with other
    components. This update ensures seamless integration and optimal
    functionality. (Issue #123)

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,10 +1,6 @@
+#!/usr/bin/pup
 # Install flask from pip3 v2.1.0
-package { 'Werkzeug' :
-  ensure   => '2.1.1',
-  provider => 'pip3'
-}
-package { 'Flask' :
+package {'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
-  require  =>  ['Werkzeug'],
 }
