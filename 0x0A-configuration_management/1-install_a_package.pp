5285d25b92f25967b07931a9039ca5ecead76e44
Madona Adeeb <madona.adeeb@hotmail.com>
2024-01-21 23:09:32 +0200

    Update Flask and add requirement for Werkzeug
    Ensure Flask is at version 2.1.0 and adds a requirement for Werkzeug at
    version 2.1.1 to resolve dependency issues. This ensures compatibility
    and stability in the package environment.
    
    Issue: #123

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,10 @@
 # Install flask from pip3 v2.1.0
+package { 'Werkzeug' :
+  ensure   => '2.1.1',
+  provider => 'pip3'
+}
 package { 'Flask' :
   ensure   => '2.1.0',
   provider => 'pip3',
+  require  =>  ['Werkzeug'],
 }
