3b30a0d27f98fa6fa8c7d3e2426a1e3f4a2d5e5c
Madona Adeeb <madona.adeeb@hotmail.com>
2024-01-30 02:37:11 +0200

    0x0F

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,8 @@
+# This script configures a new Ubuntu machine to respect the requirements asked in the task.
+exec { 'command':
+  command  => 'apt-get -y update;
+  apt-get -y install nginx;
+  sudo sed -i "/listen 80 default_server;/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default;
+  service nginx restart',
+  provider => shell,
+}
